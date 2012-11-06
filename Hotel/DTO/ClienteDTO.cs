e122e9ecc111b20672ef85da61632b9e2986068c
HotelProject <projetohotel@live.com>
2012-11-06 00:45:06 -0200

    primeiro commit

---
+++ Hotel/DTO/ClienteDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.DTO
+{
+    public class ClienteDTO
+    {
+        bool teste = true;
+        public string nome { 
+            get { return nome; } 
+            set { 
+                
+                if(teste)
+                nome = value; } }
+
+
+    }
+}
